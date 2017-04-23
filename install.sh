ac2259e0430a4fd820ab26dc5a0d57d3b360bbfd
bubbathevtog <bubbathevtog@gmail.com>
2017-04-23 11:44:41 -0400

    added update BEFORE install. Also fixed arch install

--- install.sh
+++ install.sh
@@ -4,16 +4,16 @@ getdeps(){
     if [ ! -d "/etc/python3" ]; then
 
 	if [ -d "/etc/apt/" ]; then
-	    apt-get install python3 python-pip
+	    apt-get update; apt-get install python3 python-pip
 
-	elif [ -d "/etc/pacman" ]; then
-	    pacman -S python3 python-pip
+	elif [ -d "/etc/pacman.d" ]; then
+	    pacman -Syu python3 python-pip
 
 	elif [ -d "/etc/yum" ]; then
-	    yum install python3 python-pip
+	    yum update; yum install python3 python-pip
 
 	else
-	    echo "CRIT: No packagemanager recognized..WE ARE BAILING..\n
+	    echo "CRIT: No packagemanager recognized..WE ARE BAILING..
 	    You are on your own!"
 	    exit 1
 	fi
