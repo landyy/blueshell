f1c618b14eab1e5cb027a192f690ed9d9e7f95dc
landyy <rmb2555@rit.edu>
2017-06-27 13:25:04 -0400

    Added more functionality to the terminal

---
+++ configuredb.py
@@ -0,0 +1,3 @@
+import sqlite3
+
+#check and see if the database exsists and if it doesn't, create it
--- interface.py
+++ interface.py
@@ -3,6 +3,8 @@
 import subprocess
 import os
 
+DEBUG = True
+
 class bcolors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
@@ -16,15 +18,39 @@ class bcolors:
 while True:
     
     userinput = input(bcolors.OKBLUE + "[blueshell]$ " + bcolors.ENDC)
-    
-    if(userinput == "exit"):
+    command = userinput.split()
+
+
+    if(command[0] == "exit"):
         print("[*] Exiting interface... ")
         exit()
 
-    if(userinput == "deploy"):
+    elif(command[0] == "deploy"):
         if(os.path.isfile("python_deploy.py")):
             print("[*] Running deploy script...")
             os.system("./python_deploy.py")
     
         else:
             print("[-] Deploy Script not found")
+
+    elif(command[0] == "exec"):
+        if(len(command) >= 2):
+            
+            #find the length of the array so we can pass all the arguments
+            length = len(command)
+            finalcommand=""
+            for i in range(1,length):
+                finalcommand+=command[i]
+                finalcommand+=" "
+            
+            #DEBUG statement 
+            if(DEBUG):
+                print("[!] Executing command %s" % finalcommand)
+            
+            #special case with cd. God this is such spaghetti code
+            if(command[1] == "cd"):
+                    os.chdir(command[2])
+            
+            os.system(finalcommand)
+        else:
+            print("[?] Usage: exec COMMAND")
